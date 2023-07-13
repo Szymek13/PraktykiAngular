d70d5e593c4c47e717cffaba1ce28763cbf1d6ab
Szymek13 <szymonmilewski3@gmail.com>
2023-07-13 20:32:59 +0200

    'Zmiany'

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'kanbanfire-961e0',
+    appId: '1:656625162723:web:95aef384686792026968d6',
+    databaseURL: 'https://kanbanfire-961e0-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'kanbanfire-961e0.appspot.com',
+    apiKey: 'AIzaSyA6o1qRRHHCkleIcdboEABl5v_QCcW8TKI',
+    authDomain: 'kanbanfire-961e0.firebaseapp.com',
+    messagingSenderId: '656625162723',
+    measurementId: 'G-R1KDDGNW2J',
+  },
+};
